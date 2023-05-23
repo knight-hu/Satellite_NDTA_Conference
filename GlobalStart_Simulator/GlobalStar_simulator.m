97888846740e13923228fe3730df938a8466793d
Knight <69957910+knight-hu@users.noreply.github.com>
2023-05-23 12:59:14 +0800

    Add files via upload

---
+++ GlobalStart_Simulator/GlobalStar_simulator.m
@@ -0,0 +1,35 @@
+%% 建立铱星星座
+clear all;clc;
+
+%% 全局参数
+global tStart tStop dt constellation plane_nums sats_plane No_leo No_fac No_snap Lat Long;
+tStart = 0;
+dt = 1;
+No_snap = 6846;% 运行周期
+
+%% 新建场景
+[conid] = Create_scenario();
+
+%% 创建卫星
+[parameter] = Create_satellites('GlobalStar.xlsx');
+
+%% 创建城市
+Create_facilities(conid);
+
+%% 保存数据
+[position,position_cbf] = Create_position();
+
+%% 计算延时
+inc = str2num(parameter{4,1});
+% 间隔1分钟记录一张快照
+for t = 1:60:No_snap
+    [delay] = Create_delay(position_cbf,t, inc);
+end
+
+%% 开启动画
+stkExec(conid,'Animate Scenario/globalstar_scenario Reset');
+stkExec(conid,'Animate Scenario/globalstar_scenario Start');
+
+%% 关闭连接
+stkClose(conid);
+stkClose
